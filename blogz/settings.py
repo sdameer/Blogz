187452d025747d9896f09587414cbed971c57b17
Syed_Ameer <167836628+sdameer@users.noreply.github.com>
2025-07-25 18:27:10 +0530

    Update settings.py

--- blogz/settings.py
+++ blogz/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 # STATICFILES_DIRS = [BASE_DIR / 'static']  # Create this folder if needed
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
 
