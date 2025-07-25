e8eaee8a07d836353799c887dcdae1d5d18d352c
Syed_Ameer <167836628+sdameer@users.noreply.github.com>
2025-07-25 17:49:03 +0530

    Update settings.py

--- blogz/settings.py
+++ blogz/settings.py
@@ -29,7 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+ALLOWED_HOSTS =['blogz-django-1.onrender.com']
 # ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
