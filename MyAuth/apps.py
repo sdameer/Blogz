5aed2597f7e7cfd7475e4b90fce18f9a4342c3ee
Syed_Ameer <167836628+sdameer@users.noreply.github.com>
2025-07-25 19:40:16 +0530

    Update apps.py

--- MyAuth/apps.py
+++ MyAuth/apps.py
@@ -1,6 +1,21 @@
 from django.apps import AppConfig
-
+from django.contrib.auth import get_user_model
+from django.db.utils import OperationalError, ProgrammingError
 
 class MyauthConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'MyAuth'
+    name = 'myauth'
+
+    def ready(self):
+        try:
+            User = get_user_model()
+            if not User.objects.filter(username="admin").exists():
+                User.objects.create_superuser(
+                    username="admin",
+                    email="admin@example.com",
+                    password="iamameer9393_arch"        
+                )
+                print("✅ Superuser 'admin' created.")
+        except (OperationalError, ProgrammingError):
+            # Happens during migrations or when DB is not ready
+            pass
