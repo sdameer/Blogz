8bb1da2a28c24bc4c52cf1eaa448d2711ac9227a
Syed_Ameer <syedameerdev@gmail.com>
2025-07-27 23:21:38 +0530

    settings https commit

--- blogz/settings.py
+++ blogz/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS =[
-    'https://blogz-cas6.onrender.com'
+    'blogz-cas6.onrender.com'
     "localhost",
     "127.0.0.1"
 ]
